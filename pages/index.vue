5578d564078e6f49c9577f1871b7bfb8c72b5f61
Moronfolu Olufunke <moronfolu.motunrayo1@gmail.com>
2021-08-02 14:09:54 +0100

    feat: addition of the two success and failure alert buttons

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="success">
+      Successful Transaction
+    </button>
+    <button @click="fail">
+      Failed Transaction
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    success () {
+      this.$swal('Success!', 'Transaction was successful', 'success')
+    },
+    fail () {
+      this.$swal('Error!', 'Transaction was not successful', 'error')
+    }
+  }
+}
+</script>
